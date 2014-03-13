852f07fc9d9691b8ac9f27dd8b3951e1ccee641c
Derek Overlock <overlock.derek@gmail.com>
2014-03-12 20:50:03 -0500

    - Added good Model/View example

--- js/views/ManageNetworkView.js
+++ js/views/ManageNetworkView.js
@@ -1,7 +1,6 @@
 var app = app || {};
 
 app.ManageNetworkView = Backbone.View.extend({
-
     initialize: function() {
         this.render();
         this.listenTo(this.model, 'change', this.render);
@@ -12,7 +11,11 @@ app.ManageNetworkView = Backbone.View.extend({
         return Mustache.render(app.TemplateManager.getTemplate('manage_network'), param);
     },
     events: {
-
+        'click button' : 'save'
+    },
+    save: function(event) {
+        var form = $(event.target).closest("form");
+        alert('Coming soon...');
     },
     render: function() {
         this.$el.html(
