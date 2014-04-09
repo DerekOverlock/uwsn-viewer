7cbfd0931bbd010163907443f1efc8adb513fe94
Derek Overlock <overlock.derek@gmail.com>
2014-04-08 21:46:32 -0500

    Added controllers

--- scripts/addNodeToNetwork.php
+++ scripts/addNodeToNetwork.php
@@ -3,12 +3,11 @@ require_once __DIR__ . "/../config.inc.php";
 require_once PHP_LIB . "/Node.php";
 
 $networkId = $_POST['networkId'];
-$name = $_POST['name'];
 $lat = $_POST['lat'];
 $long = $_POST['long'];
 $alt = $_POST['alt'];
 
-$node = Node::AddNode($name, $lat, $long, $alt, $networkId);
+$node = Node::AddNode("", $lat, $long, $alt, $networkId);
 
 /**
 $node = Node::getNode($node->getId());
